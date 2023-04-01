3e0d389dec6962dff2e8de77d6a613589b48ebd9
It'z ArU <88024858+Aru-Ofc-git@users.noreply.github.com>
2023-04-02 01:53:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = expreess();
 const port = process.env.PORT || 8005;
 app.get("/", (req, res) => {
     res.send("hello world");
+});
 app.listen(port, () => {
     console.log("server running");
 })
