348313c1f8d9317611f9a704ddc6f20028345b5b
It'z ArU <88024858+Aru-Ofc-git@users.noreply.github.com>
2023-04-02 01:46:10 +0600

    Update and rename app.js to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const expreess = require("express");
+const app = expreess();
+const port = process.env.PORT || 8005;
+app.get("/", (req, res) => {
+    res.send("hello world");
+app.listen(port, () => {
+    console.log("server running");
+})
